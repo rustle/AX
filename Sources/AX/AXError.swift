b3522bea4e90de4907afc08ed0f21c57be3dfeba
Doug Russell <doug@getitdownonpaper.com>
2022-01-02 15:08:07 -0500

    Add comments to Error cases

--- Sources/AX/AXError.swift
+++ Sources/AX/AXError.swift
@@ -11,20 +11,37 @@ import ApplicationServices.HIServices
 
 @available(macOS 10.2, *)
 public enum Error: Swift.Error {
+    /// The action is not supported by the UIElement.
     case actionUnsupported
+    /// The accessibility API is disabled.
+    ///
+    /// See also `isTrusted(promptIfNeeded:)`
     case apiDisabled
+    /// The attribute is not supported by the UIElement.
     case attributeUnsupported
+    /// The parameterized attribute is not supported by the UIElement.
     case parameterizedAttributeUnsupported
+    /// The function cannot complete because messaging failed in some way or because the application with which the function is communicating is busy or unresponsive.
     case cannotComplete
+    /// A system error occurred, such as the failure to allocate an object.
     case failure
+    /// An illegal argument was passed to the function.
     case illegalArgument
+    ///  The AXUIElementRef passed to the function is invalid.
     case invalidUIElement
+    /// The Observer passed to the function is not a valid observer.
     case invalidUIElementObserver
+    /// Not enough precision.
     case notEnoughPrecision
+    /// This notification has already been registered.
     case notificationAlreadyRegistered
+    /// The notification is not supported by the UIElement
     case notificationUnsupported
+    /// Indicates that the function or method is not implemented (this can be returned if a process does not support the accessibility API).
     case notImplemented
+    /// Indicates that a notification is not registered yet.
     case notificationNotRegistered
+    /// The requested value or UIElement does not exist.
     case noValue
     public init?(error: AXError) {
         switch error {
