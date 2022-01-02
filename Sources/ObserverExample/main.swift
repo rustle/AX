2226afc0d7ec11c50622b753af5e01c4d2bc9deb
Doug Russell <doug@getitdownonpaper.com>
2022-01-02 15:13:46 -0500

    Debugging tip

--- Sources/ObserverExample/main.swift
+++ Sources/ObserverExample/main.swift
@@ -8,6 +8,12 @@ import AX
 import Cocoa
 
 guard isTrusted(promptIfNeeded: true) else {
+    // If you already added ObserverExample
+    // to trusted apps, it's likely that changing the
+    // binary has invalidated it's AX API access.
+    // You can usually reauthorize it by unchecking and
+    // rechecking it's entry in the list of apps
+    // with AX API access in System Preferences.
     print("Not Trusted")
     exit(1)
 }
